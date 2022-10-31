04c464bf46a73e7396b2c6d2d0604a87c98da539
Nnaemeka <Nnaemekaonyeokoro@outlook.com>
2022-10-31 01:29:44 -0700

    Renamed to app.jsx and refreshed on some core rn stuff

---
+++ App.jsx
@@ -0,0 +1,21 @@
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View>
+        <TextInput placeholder="your course goal..." />
+        <Button title="add goal" />
+      </View>
+      <View>
+        <Text>List of goals...</Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 100,
+  },
+});
